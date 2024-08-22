43b619f7e84c9ae53f1c68c9ce6300cf58ce7982
patrivar <patric.varis@metropolia.fi>
2024-08-22 15:18:04 +0300

    Python tehtävät

---
+++ mod02/teht2.py
@@ -0,0 +1,13 @@
+#pii*r potenssiin -> **2 ( * on kerto ja ** on potenssi)
+import random
+
+r = float(input("Anna ympyrän säde (m): "))
+import math
+area = math.pi * r **2
+print(f"Ympyrä, jonka säde on {r}, piiinta-ala on {area:.1f} neliömetriä.")
+
+
+
+# satunnaisen kokonaisluvun arpominen
+random_number = random.randint(1,6)
+print(f"Satunnainen luku 1-6: {random_number}")
