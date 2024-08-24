6b66688c9f6096e102f8030b465a701180b37330
patrivar <patric.varis@metropolia.fi>
2024-08-24 18:25:18 +0300

    Python tehtävät

--- mod02/teht5.py
+++ mod02/teht5.py
@@ -3,7 +3,6 @@
 #1 leiviskä on 20 naulaa
 #1 naula on 32 luotia
 #1 luoti on 13.3g
-from getopt import gnu_getopt
 
 l = 13.3
 n = 32 * (l)
@@ -11,11 +10,19 @@ lei = 20 * (n)
 
 le = float(input("Anna leivisköiden määrä: "))
 na = float(input("Anna naulojen määrä: "))
-luo = float(input("Anna luotien määrä"))
+luo = float(input("Anna luotien määrä: "))
 
-summa = (luo * l + n * na + lei * le)/1000
+summa = (luo * l + n * na + lei * le)
 
 
-print(f"Massa on nykymittojen mukaan: {summa:.3f}kg")
+print(f"Massa on nykymittojen mukaan: {summa}")
+
+
+
+weight_in_grams = summa % 1000
+weight_in_kilograms = int(summa / 1000)
+
+print(f"Kokonaispaino on {weight_in_kilograms:.0f}kg ja {weight_in_grams:.2f}g")
+
 
 
