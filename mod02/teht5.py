00d2ef629b582c585e853f20a8fa4f5688e49351
patrivar <patric.varis@metropolia.fi>
2024-08-26 09:36:38 +0300

    Python tehtävät

--- mod02/teht5.py
+++ mod02/teht5.py
@@ -20,6 +20,6 @@ print(f"Massa on nykymittojen mukaan: {summa}")
 
 
 weight_in_grams = summa % 1000
-weight_in_kilograms = int(summa / 1000)
+weight_in_kilograms = summa // 1000
 
 print(f"Kokonaispaino on {weight_in_kilograms:.0f}kg ja {weight_in_grams:.2f}g")
