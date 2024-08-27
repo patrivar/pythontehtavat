b10c6323fdc948a0359caf41496b42335bd49dec
patrivar <patric.varis@metropolia.fi>
2024-08-27 14:09:53 +0300

    Python tehtävät

--- mod04/mod4-esim.py
+++ mod04/mod4-esim.py
@@ -36,6 +36,15 @@ while command != "lopeta":
     if command == "lopeta":
         print("Lopetetaan.")
         #break# "Heittää ulos" silmukasta, ei paras ohjelmointikäytäntö
+
+    elif command == "kukkuu":
+        max_count = int(input("Montako kertaa kukutaan? "))
+        counter = 0
+        while counter < max_count:
+            counter = counter + 1
+            print(f"{counter}. Kerran Kukkuu")
+        print(f"Laskurin arvo lopuksi {counter}")
+
     else:
         print("En ymmärrä komentoa. Yritä uudestaan.")
 print("Ohjelma sammutettu.")
