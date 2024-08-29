3a88c9e167f663d6501859a661797be16bccb6f1
patrivar <patric.varis@metropolia.fi>
2024-08-29 15:31:26 +0300

    Python tehtävät

--- mod04/teht5.py
+++ mod04/teht5.py
@@ -7,17 +7,15 @@
 t = input("Anna käyttäjä tunnus: ")
 s = input("Anna salasana: ")
 k = 1
-while k <= 5:
-    if k == 5:
-        print("Pääsy evätty")
-        break
-
-    elif t != "python" or s != "rules":
-        print("Käyttäjätunnus tai salasana väärin. Yritä uudestaan.")
-        t = input("Anna käyttäjä tunnus: ")
-        s = input("Anna salasana: ")
-        k += 1
-
-    elif t == "python" and s == "rules":
-        print("Tervetuloa")
-        break
+if t == "python" and s == "rules":
+    print("Tervetuloa.")
+while t != "python" or s != "rules":
+            if k < 5:
+             print("Käyttäjätunnus tai salasana väärin. Yritä uudestaan.")
+             t = input("Anna käyttäjä tunnus: ")
+             s = input("Anna salasana: ")
+             k += 1
+             if k == 5:
+                 print("Pääsy evätty.")
+else:
+    print("Tervetuloa.")
