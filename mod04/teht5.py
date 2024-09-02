1668ef2b5782fa39a19fe7a8c31cb89deb1eab56
patrivar <patric.varis@metropolia.fi>
2024-09-02 09:11:03 +0300

    Python tehtävät

--- mod04/teht5.py
+++ mod04/teht5.py
@@ -1,22 +1,14 @@
-# Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen ja salasanan.
-# Jos jompikumpi tai molemmat ovat väärin, tunnus ja salasana kysytään uudelleen.
-# Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa.
-# Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty.
-# (Oikea käyttäjätunnus on python ja salasana rules).
-
-t = input("Anna käyttäjä tunnus: ")
-s = input("Anna salasana: ")
+t = "python"
+s = "rules"
+inputt = input("Anna käyttäjätunnus")
+inputs = input("Anna salasana")
 k = 1
 
-while t != "python" or s != "rules":
-            if k <5:
-                print("Käyttäjätunnus tai salasana väärin. Yritä uudestaan.")
-                t = input("Anna käyttäjä tunnus: ")
-                s = input("Anna salasana: ")
-            k += 1
-            if t == "python" and s == "rules":
-                print("Tervetuloa.")
-
-            if k ==5:
-                print("Pääsy evätty")
-                break
+while (inputt != t or inputs != s) and k < 5:
+    inputt = input("Anna käyttäjätunnus")
+    inputs = input("Anna salasana")
+    k += 1
+if k == 5:
+    print("Pääsy evätty")
+else:
+    print("Tervetuloa")
