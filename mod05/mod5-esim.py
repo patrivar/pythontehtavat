00b78c8339b22c29e6c3c0f8fa2552662a86fddf
patrivar <patric.varis@metropolia.fi>
2024-09-02 11:06:03 +0300

    Python tehtävät

--- mod05/mod5-esim.py
+++ mod05/mod5-esim.py
@@ -20,9 +20,10 @@ print(f"Listan pituus on: {lenght}")
 
 '''
 #Alkioon viitataan indeksinummerolls
-print(f"Hei {nimet[3]}, ikäsi on {iat[3]} vuotta.")
+print(f"Hei {nimet[3:4]}, ikäsi on {iat[3:4]} vuotta.")
 '''
 
+'''
 #Listan läpikäynti while silmukan avulla
 iterator = 0
 #While 0 < 5:
@@ -34,4 +35,56 @@ iterator = 0
 while iterator < len(nimet):
     print(f"Hei {nimet[iterator]}, ikäsi on {iat[iterator]} vuotta.")
     iterator += 1
+'''
+'''
+#nimet = ["Nikku", "Cassu", "Crisu", "Lumi", "Tiku"]
+#iat = ["15", "23", "25", "13", "20"]
+print(nimet[2:4]) # 2, 3 (ei viimeistä alkiota) 2 alkiota indeksistä 2 alkaen
+print(nimet[2:]) # Antaa kaikki alkiot indeksilä 2 alkaen
+print(nimet[-1]) # Listan viimeinen alkio
+print(nimet[-2]) # Antaa alkion toiseksi viimeisen alkion
+print(nimet[-1:-3:-1]) #Halutut alkiot käänteis järjestyksessä
+#print(nimet[2:6:2]) # indeksistä 2 indekssiin 6, joka toinen
+'''
+'''
+#Listaan voi yhdistää ja siellä voi olla erillaisia tietorakenteita
+Lista1 = ["ulla", "Matti", "Ilkka"]
+yhdistetty_lista =  [23, 54,25,100,22, Lista1]
+print(yhdistetty_lista)
+print(yhdistetty_lista[5][0])
+'''
 
+'''
+names = []
+
+nimi = input("Anna ensimmäinen nimi tai lopeta painamalla Enter: ")
+while nimi != "":
+    nimet.append(nimi)
+    nimi = input("Anna seuraava nii tai lopeta painamlla Enter: ")
+
+print(names)
+'''
+
+#nimet = ["Nikku", "Cassu", "Crisu", "Lumi", "Tiku"]
+
+nimet.append("uusi nimi") #Lisää uuden alkion listan loppuun
+print(nimet)
+nimet.insert(0, "kala")
+print(nimet)
+print(nimet.index("Tiku"))
+if "uusi nimi" in nimet:
+    print("uusi nimi ja kala löytyi listasta ja nyt poistetaan ne")
+    nimet.remove("uusi nimi")
+    nimet.remove("kala")
+    print(nimet)
+print(nimet.index("Tiku"))
+
+more_names = ("Iines", "Aku", "Roope", "Milla")
+nimet.extend(more_names)
+print(nimet)
+#uusilista = nimet + more_names
+
+nimet[3] = "Lumi-Helmi"
+print(nimet)
+
+print(nimet.sort())
