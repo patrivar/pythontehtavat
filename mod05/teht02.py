9375f25d7b878b4fc44bb50c3eb7609b433b0e52
patrivar <patric.varis@metropolia.fi>
2024-09-03 20:14:25 +0300

    Python tehtävät

--- mod05/teht02.py
+++ mod05/teht02.py
@@ -1,3 +1,15 @@
-# Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
-# Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen.
+# Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon
+# lopetusmerkiksi. Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä
+# suurimmasta alkaen.
 # Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille argumentiksi reverse=True.
+
+l = []
+k = l.append(input("Anna luku: "))
+
+while k != "":
+    k = (input("Anna luku: "))
+    if k != "":
+        l.append(k)
+        l.sort(reverse=True)
+else:
+    print(l)
