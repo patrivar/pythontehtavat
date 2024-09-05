033df43c804382aff4e1ba20be730a030b15cde6
patrivar <patric.varis@metropolia.fi>
2024-09-05 14:42:05 +0300

    Python tehtävät

--- mod06/teht3.py
+++ mod06/teht3.py
@@ -3,3 +3,18 @@
 # ja muuntaa sen litroiksi. Muunnos on tehtävä aliohjelmaa hyödyntäen. Muuntamista jatketaan siihen saakka,
 # kunnes käyttäjä syöttää negatiivisen gallonamäärän.
 # Yksi gallona on 3,785 litraa.
+
+
+g = int(input("Montako gallonaa bensaa tynnyrissä on: "))
+
+def tynnyri(g):
+    if g >= 0:
+        while g >= 0:
+            l = g * 3.785
+            print(f"Tynnyrissä on siis {l}")
+            g = int(input("Montako gallonaa bensaa tynnyrissä on: "))
+
+    print("Tynnyrissä ei ole polttoainetta")
+    return True
+
+tynnyri(g)
