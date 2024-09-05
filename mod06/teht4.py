473925a0bfc6e795082ca60a75440312ab6e1cd5
patrivar <patric.varis@metropolia.fi>
2024-09-05 14:58:05 +0300

    Python tehtävät

--- mod06/teht4.py
+++ mod06/teht4.py
@@ -1,3 +1,17 @@
 # Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja. Ohjelma palauttaa listassa olevien
 # lukujen summan. Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat
 # sen palauttaman summan.
+
+l = []
+k = input("Anna luku: ")
+
+while k != "":
+    l.append(k)
+    k = input("Anna luku: ")
+
+def sum(*l):
+        tsum = 0
+        for sum in l:
+            tsum = tsum + int(sum)
+        return tsum
+print(sum(*l))
