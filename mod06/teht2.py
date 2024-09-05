71611991ac9ec41c61db54819b0cc21c748164ef
patrivar <patric.varis@metropolia.fi>
2024-09-05 13:59:09 +0300

    Python tehtävät

--- mod06/teht2.py
+++ mod06/teht2.py
@@ -2,3 +2,20 @@
 # Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa.
 # Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan nopan
 # maksimisilmäluku, joka kysytään käyttäjältä ohjelman suorituksen alussa.
+
+import random
+
+n = int(input("Montako tahkoista noppaa heitetään: "))
+
+def noppa(n):
+    s = random.randint(1,n)
+    if s == n:
+        print(f"{s}")
+        return True
+    else:
+       while s != n:
+           s = random.randint(1,n)
+           print(f"{s}")
+    return True
+
+noppa(n)
