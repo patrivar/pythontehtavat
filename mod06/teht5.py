3fead78967739a08dcf7de4c5eccc2608c057e9a
patrivar <patric.varis@metropolia.fi>
2024-09-05 15:25:18 +0300

    Python tehtävät

--- mod06/teht5.py
+++ mod06/teht5.py
@@ -2,3 +2,12 @@
 # joka on muuten samanlainen kuin parametrina saatu lista paitsi että siitä on karsittu pois kaikki
 # parittomat luvut. Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat
 # sen jälkeen sekä alkuperäisen että karsitun listan.
+
+l = []
+k = []
+
+
+def ero(k):
+    for i in range(30):
+        l.append(i)
+        k.append(i)
