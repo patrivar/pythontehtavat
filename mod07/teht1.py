bc1c37cc423e8c5d74b9e682a4ca2cbd1c51c085
patrivar <patric.varis@metropolia.fi>
2024-09-10 11:58:00 +0300

    Python tehtävät

--- mod07/teht1.py
+++ mod07/teht1.py
@@ -3,3 +3,18 @@
 # merkkijonoina monikkotietorakenteeseen. Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten,
 # että joulukuu on ensimmäinen talvikuukausi.
 
+kuukausi = ("Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu",
+            "Kesäkuu", "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu")
+
+
+k = int(input("Monesko kuukausi on menossa? (1-12) "))
+kuukausi = kuukausi[k-1]
+
+if k == (2,3,4):
+    print("Kevättä eletään.")
+elif k == (5,6,7):
+    print("Elätään kesä aikaa.")
+elif k == (8,9,10):
+    print("Eletään kaunista syksyä.")
+else:
+    print("Talvikausi meneillään.")
