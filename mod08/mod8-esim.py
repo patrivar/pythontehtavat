94fccae45bb47fb2acb2b2215067f77695b71f23
patrivar <patric.varis@metropolia.fi>
2024-09-10 14:35:46 +0300

    Python tehtävät

---
+++ mod08/mod8-esim.py
@@ -0,0 +1,14 @@
+
+import mysql.connector
+
+#connect()-funktio palauttaa tietokantayhteyden, joka sijoitetaan muuttujaan
+
+connection = mysql.connector.connect(
+        host='127.0.0.1', #localhost
+        port=3306,
+        database='flight_game',
+        user='lentokentta',
+        password='lentokentta',
+        autocommit=True,
+        collation='utf8mb4_general_ci',
+        )
