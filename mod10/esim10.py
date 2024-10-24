d5d48c183ed2f86dacbb84444c97a7658b5f6e35
patrivar <patric.varis@metropolia.fi>
2024-10-24 15:15:41 +0300

    Python tehtävät

--- mod10/esim10.py
+++ mod10/esim10.py
@@ -1,10 +1,18 @@
+import random
 class Autotalli:
     def __init__(self):
         self.autot = []
     def auto_sisaan(self, auto):
+        # Tarkistetaan ettei auto ole jo tallissa
+        # Parempi tapa olisi muuttaa lista joukoksi, jolloin dublikaatteja ei sallita
+        for a in self.autot:
+            if a == auto: # => True jos viittaavat samaan olioon. Eli olio on jo tallissa.
+                return
         self.autot.append(auto)
-    def auto_ulos(self):
-        pass
+
+    def auto_ulos(self,auto):
+        self.autot.remove(auto)
+
     def tulosta_inventaario(self):
         print("Autotallissa on:")
         for auto in self.autot:
@@ -20,8 +28,10 @@ class Kuljettaja:
         print(f"Olen{self.nimi}, {self.ika} ja ajan autoa {self.auto.rek_nro}")
         self.auto.kiihdytä(40)
         print(self.auto.nopeus)
+        self.auto.kulje(1)
         self.auto.kiihdytä(140)
         print(self.auto.nopeus)
+        self.auto.kulje(0.1)
         self.auto.kiihdytä(-250)
         self.auto.tulosta_ominaisuudet
 
@@ -29,11 +39,13 @@ class Auto:
     def __init__(self, rek_nro, huippunopeus):
         self.rek_nro = rek_nro
         self.nopeus = 0
+        self.matka = 0
         self.huippunopeus = huippunopeus
+        print(f"Auto luotu {self.rek_nro}, huiput {self.huippunopeus}")
 
     def tulosta_ominaisuudet(self):
         print(f"{self.rek_nro}, huippunopeus: {self.huippunopeus}")
-        print(f"Tämän hetkinen nopeus: {self.nopeus}")
+        print(f"Nopeus: {self.nopeus}, matkamittari: {self.matka}")
 
     def kiihdytä(self, nopeuden_muutos):
         self.nopeus = self.nopeus + nopeuden_muutos
@@ -41,6 +53,11 @@ class Auto:
             self.nopeus = self.huippunopeus
         if self.nopeus < 0:
             self.nopeus = 0
+
+    def kulje(self, aika):
+        #Aika tunneissa
+        self.matka += aika * self.nopeus
+
 
 # Luodaan olio ja sijoitetaan viittaukset niihin muuttujiin
 
@@ -50,9 +67,22 @@ kuski1 = Kuljettaja('Räikkönen', 48, a1)
 kuski1.aja()
 
 # Luodaan autotalli-tyyppinen olio ja sijoitetaan autot sinne
+
 talli = Autotalli()
+
 talli.auto_sisaan(a1)
+#talli.auto_sisaan(a2)
+#talli.tulosta_inventaario()
+#talli.auto_ulos(a1)
+#talli.tulosta_inventaario()
+
+
+#Luodaan 10 erillaista auto-oliota autotalliin
+for i in range(10):
+    uusi_auto = Auto(f"ABC-{i+1}", random.randint(100,200))
+    talli.auto_sisaan(uusi_auto)
 talli.tulosta_inventaario()
+
 
 '''
 a1.kiihdytä(-55)
