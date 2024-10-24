363e5f35cf9b136bb8122e189d1f3e893ed66693
patrivar <patric.varis@metropolia.fi>
2024-10-24 17:10:56 +0300

    Python tehtävät

---
+++ mod09/teht3.py
@@ -0,0 +1,4 @@
+# Laajenna ohjelmaa siten, että mukana on kulje-metodi, joka saa parametrinaan tuntimäärän.
+# Metodi kasvattaa kuljettua matkaa sen verran kuin auto on tasaisella vauhdilla annetussa
+# tuntimäärässä edennyt. Esimerkki: auto-olion tämänhetkinen kuljettu matka on 2000 km. Nopeus
+# on 60 km/h. Metodikutsu auto.kulje(1.5) kasvattaa kuljetun matkan lukemaan 2090 km.
---
+++ mod10/teht3.py
@@ -0,0 +1,3 @@
+# Jatka edellisen tehtävän ohjelmaa siten, että Talo-luokassa on parametriton metodi
+# palohälytys, joka käskee kaikki hissit pohjakerrokseen. Jatka pääohjelmaa siten, että
+# talossasi tulee palohälytys.
