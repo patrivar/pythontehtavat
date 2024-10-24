1655fcb4bde8fe22c6c894f75da4d2b31f7b497e
patrivar <patric.varis@metropolia.fi>
2024-10-24 17:51:18 +0300

    Python tehtävät

--- mod09/teht1.py
+++ mod09/teht1.py
@@ -9,11 +9,8 @@ class Auto:
     def __init__(self, rek_nro, huippunopeus):
         self.rek_nro = rek_nro
         self.nopeus = 0
+        self.matka = 0
         self.huippunopeus = huippunopeus
+        print(f"Auto luotu {self.rek_nro}, huiput {self.huippunopeus}")
 
-    def tulosta_rekkari(self):
-        print(f'Rekkari: {self.rek_nro}')
-
-
-Auto('ABC-123', 120).tulosta_rekkari()
-Auto('XYZ-789', 250)
+Auto('ABC-123', 142)
--- mod09/teht2.py
+++ mod09/teht2.py
@@ -6,3 +6,29 @@
 # hätäjarrutus määräämällä nopeuden muutos -200 km/h ja tulosta uusi nopeus. Kuljettua matkaa ei
 # tarvitse vielä päivittää.
 
+class Auto:
+    def __init__(self, rek_nro, huippunopeus):
+        self.rek_nro = rek_nro
+        self.nopeus = 0
+        self.matka = 0
+        self.huippunopeus = huippunopeus
+        print(f"Auto luotu {self.rek_nro}, huiput {self.huippunopeus}")
+
+    def kiihdyta(self, nopeuden_muutos):
+        self.nopeus = self.nopeus + nopeuden_muutos
+        if self.nopeus > self.huippunopeus:
+            self.nopeus = self.huippunopeus
+        if self.nopeus < 0:
+            self.nopeus = 0
+
+    def auton_nopeus(self):
+        print(f"Nopeus: {self.nopeus}")
+
+
+a1 = Auto('ABC-123', 142)
+a1.kiihdyta(30)
+a1.kiihdyta(70)
+a1.kiihdyta(50)
+a1.auton_nopeus()
+a1.kiihdyta(-200)
+a1.auton_nopeus()
