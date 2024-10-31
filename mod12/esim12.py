6342d3d75302d0707db3bdf2466c7e43677e610a
patrivar <patric.varis@metropolia.fi>
2024-10-31 13:56:21 +0200

    Python tehtävät

---
+++ mod12/esim12.py
@@ -0,0 +1,4 @@
+
+import requests
+
+respond = request.get('https://www.tvmaze.com/search/shows?q=emmerdale')
--- mod12/teht1.py
+++ mod12/teht1.py
@@ -1,3 +1,4 @@
 # Kirjoita ohjelma, joka hakee ja tulostaa satunnaisen Chuck Norris -vitsin käyttäjälle.
 # Käytä seuravalla sivulla esiteltävää rajapintaa: https://api.chucknorris.io/. Käyttäjälle
 # on näytettävä pelkkä vitsin teksti.
+
